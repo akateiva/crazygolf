71ab1c008e485d0210bcdce58a67e3b3e3dbf4e0
Roger <r.sijben@outlook.com>
2016-05-27 04:08:34 +0200

    ui work

--- core/src/com/group9/crazygolf/menu/NewGameWindow.java
+++ core/src/com/group9/crazygolf/menu/NewGameWindow.java
@@ -15,51 +15,91 @@ class NewGameWindow extends Window {
     NewGameData data;
     Table playerTable;
     Skin skin;
+    final MenuScreen screen;
+    final Label textLabel;
+    private boolean playersAdded = false;
+    private boolean courseSelected = false;
 
-    NewGameWindow(Skin skin, final crazygolf game) {
+    NewGameWindow(Skin skin, final crazygolf game, MenuScreen screen) {
         super("New Game", skin);
-        setModal(true);
-
+        this.setModal(true);
         this.skin = skin;
+        this.setSize(400, 400);
+        this.screen = screen;
+
+        textLabel = new Label("Step 1: Add player(s)", skin);
+
+        row();
+        add(textLabel).left().padBottom(20);
+        row().height(100);
 
         playerTable = new Table(skin);
         playerTable.top();
 
-
-        add(playerTable).expandX();
+        add(playerTable);
 
         data = new NewGameData();
 
-        row();
+        row().height(30);
+
+        int buttonWidth = 120;
+        int padding = 60;
+
+        // Define buttons
         TextButton addPlayerButton = new TextButton("Add player", skin);
+        TextButton cancelButton = new TextButton("Cancel", skin);
+        TextButton startButton = new TextButton("Start", skin);
+        TextButton courseButton = new TextButton("Select course", skin);
+
+        // Add buttons
+        add(addPlayerButton).left().width(buttonWidth).padRight(padding);
+        add(courseButton).left().width(buttonWidth);
+        row().height(30);
+        add(cancelButton).left().width(buttonWidth).padTop(padding);
+        add(startButton).left().width(buttonWidth).padTop(padding);
+
+        // Add listeners
         addPlayerButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 addNewPlayer();
             }
         });
-        add(addPlayerButton).expandX().pad(10);
-
-        TextButton cancelButton = new TextButton("Cancel", skin);
         cancelButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
                 remove();
             }
         });
-        add(cancelButton).expandX().pad(10);
-
-        TextButton startButton = new TextButton("Start", skin);
         startButton.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                game.setScreen(new GameScreen(game, data, Gdx.files.local("courses/assfuckery")));
-                remove();
+                if (playersAdded && courseSelected) {
+                    game.setScreen(new GameScreen(game, data, Gdx.files.local("courses/assfuckery")));
+                    remove();
+                }
+                if (playersAdded == false && courseSelected == false || playersAdded == false && courseSelected == true) {
+                    textLabel.setText("Error: No player(s) added");
+                }
+                if (playersAdded == true && courseSelected == false) {
+                    textLabel.setText("Error: No course selected");
+                }
             }
         });
-        add(startButton).expandX().pad(10);
+        courseButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                showFileChooser();
+            }
+        });
+    }
 
-        pack();
+    public void setCourseSelected() {
+        courseSelected = true;
+    }
+
+    private void showFileChooser() {
+        screen.chooseFile();
     }
 
     private void updatePlayerTable() {
@@ -86,7 +126,13 @@ class NewGameWindow extends Window {
             playerTable.add(removePlayer).pad(5);
             playerTable.row();
         }
-        pack();
+        if (data.getPlayerList().size() == 0) {
+            textLabel.setText("Step 1: Add player(s)");
+            playersAdded = false;
+        } else {
+            textLabel.setText("Step 2: Select course");
+            playersAdded = true;
+        }
     }
 
     private void addNewPlayer() {
