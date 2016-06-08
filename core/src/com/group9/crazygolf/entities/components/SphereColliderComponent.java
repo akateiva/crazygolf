dfb45c3bc0cc0f162f0c9b76ff9f77d5a267f063
akateiva <kateivaaleksas@gmail.com>
2016-06-08 14:27:14 +0200

    cleanup

--- core/src/com/group9/crazygolf/entities/components/SphereColliderComponent.java
+++ core/src/com/group9/crazygolf/entities/components/SphereColliderComponent.java
@@ -6,5 +6,5 @@ import com.badlogic.ashley.core.Component;
  * Created by akateiva on 08/05/16.
  */
 public class SphereColliderComponent implements Component {
-    public float radius = 1f;
+    public float radius;
 }
